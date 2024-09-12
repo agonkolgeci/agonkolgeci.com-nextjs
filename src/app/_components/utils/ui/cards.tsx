13033c5b606427e9bb48b38bc37178194e67dd78
Agon <agon-kolgeci@hotmail.com>
2024-09-12 22:04:13 +0200

    Update cards.tsx

--- src/app/_components/utils/ui/cards.tsx
+++ src/app/_components/utils/ui/cards.tsx
@@ -13,18 +13,24 @@ export function CardImage({ className, border, ...props } : { className: string,
 
 export function CardContainer({ orientation = Orientation.VERTICAL, justify = Justify.START, children } : { orientation?: Orientation, justify?: Justify, children: React.ReactNode }): React.ReactNode {
     return (
-        <div className={`flex flex-col ${justify} lg:${orientation} gap-6 p-6 w-full h-full relative`} children={children} />
+        <div className={`flex flex-col ${justify} lg:${orientation} gap-6 p-6 w-full h-full relative`} >
+            {children}
+        </div>
     )
 }
 
 export function Card({ orientation = Orientation.VERTICAL, animation = Animation.NONE, children } : { orientation?: Orientation, animation?: Animation, children: React.ReactNode }): React.ReactNode {
     return (
-        <li className={`flex flex-col justify-between lg:${orientation} w-full h-full bg-white shadow-xl rounded-2xl ${animation}`} children={children} />
+        <li className={`flex flex-col justify-between lg:${orientation} w-full h-full bg-white shadow-xl rounded-2xl ${animation}`}>
+            {children}
+        </li>
     )
 }
 
 export function Cards({ className, children } : { className: string, children: React.ReactNode }) {
     return (
-        <ul className={`grid justify-center w-full gap-20 ${className}`} children={children} />
+        <ul className={`grid justify-center w-full gap-20 ${className}`}>
+            {children}
+        </ul>
     )
 }
