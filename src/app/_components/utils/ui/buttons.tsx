6f897edd0742284d3147cca68cbe7b182762baa1
Agon <agon-kolgeci@hotmail.com>
2024-09-12 23:56:53 +0200

    Fix button color

--- src/app/_components/utils/ui/buttons.tsx
+++ src/app/_components/utils/ui/buttons.tsx
@@ -1,7 +1,7 @@
 import { Animation } from "./render";
 
 export enum ButtonStyle {
-    PRIMARY = "border-primary border-2 bg-white text-black disabled:bg-gray-500 px-8 py-2 rounded-full hover:bg-primary hover:text-white",
+    PRIMARY = "border-primary border-2 bg-white text-primary disabled:bg-gray-500 px-8 py-2 rounded-full hover:bg-primary hover:text-white",
     ACTION = "text-gray-500 hover:text-green-500"
 }
 
