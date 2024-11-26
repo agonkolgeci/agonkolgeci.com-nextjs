52a1ab58ef5202f35c068b6d39b597a4917e3ea1
Agon <agon-kolgeci@hotmail.com>
2024-11-26 13:44:30 +0100

    Page 404 translated

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,17 @@
+import { useTranslations } from "next-intl";
 import Image from "next/image"
+import { Button, ButtonStyle } from "./_components/utils/ui/Button";
 
 export default function NotFound() {
+  const t = useTranslations("errors.404");
+  
   return (
     <div className="flex flex-col md:flex-row items-center justify-center w-full py-28 gap-16 bg-white text-black">
-      <div className="flex flex-col">
-        <h1 className="font-bold text-8xl">404</h1>
-        <p className="text-lg">Page not found</p>
-        <p className="text-xs">The page you are looking for doesn't exist.</p>
+      <div className="flex flex-col max-w-screen-sm">
+          <h1 className="font-bold text-8xl">404</h1>
+
+          <p className="text-xl">{t("title")}</p>
+          <p className="text-base animate-pulse">{t("description")}</p>
       </div>
 
       <Image src={"/errors/404.webp"} width={256} height={256} alt="Robot 404"/>
